96a9a356c91a03bc79c7218798557f9e77c3ef85
ChristianS <cdsanchezc96@gmail.com>
2024-04-06 15:24:23 -0600

    adding dotnet project

--- PushFirebaseDotnet/PushFirebaseDotnet/Startup.cs
+++ PushFirebaseDotnet/PushFirebaseDotnet/Startup.cs
@@ -1,12 +1,19 @@
 using FiebaseManager.Manager;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace pushNotificationsDotNetFirebase
+namespace PushFirebaseDotnet
 {
     public class Startup
     {
@@ -17,28 +24,33 @@ namespace pushNotificationsDotNetFirebase
 
         public IConfiguration Configuration { get; }
 
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<IFirebaseNotificationsRepository, FirebaseNotificationsRepository>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "pushNotificationsDotNetFirebase", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "PushFirebaseDotnet", Version = "v1" });
             });
         }
 
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "pushNotificationsDotNetFirebase v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PushFirebaseDotnet v1"));
             }
 
             app.UseHttpsRedirection();
+
             app.UseRouting();
+
             app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
--- PushFirebaseDotnet/PushFirebaseDotnet/WeatherForecast.cs
+++ PushFirebaseDotnet/PushFirebaseDotnet/WeatherForecast.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace pushNotificationsDotNetFirebase
+namespace PushFirebaseDotnet
 {
     public class WeatherForecast
     {
